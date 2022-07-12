f37d1a75112c63fbba9662bb70b44910d449ce0c
R1425 <108553492+R1425@users.noreply.github.com>
2022-07-12 19:53:55 -0400

    user choice

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
+# this is the "game.py" file...
+
+print("Rock, Paper, Scissors, Shoot!")
 
 
 
-#
-# todo: write some Python code here to satisfy the exercise objectives
-# ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
+# USER INPUTS
+
+
+user_choice = input("Please make a selection ('rock' 'paper', 'scissors'):")
+
+#You chose: 'rock'
+print("You chose:", user_choice)
+print(f"You chose: '{user_choice}' ")
