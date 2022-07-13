dc98f505767ec3ee38a8608cfa1f2ff0bb8e9197
R1425 <108553492+R1425@users.noreply.github.com>
2022-07-12 20:31:31 -0400

    Computer Choice

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-# this is the "game.py" file...
+import random
+
 
 print("Rock, Paper, Scissors, Shoot!")
 
@@ -12,3 +13,14 @@ user_choice = input("Please make a selection ('rock' 'paper', 'scissors'):")
 #You chose: 'rock'
 print("You chose:", user_choice)
 print(f"You chose: '{user_choice}' ")
+
+# VALIDATE USER INPUTS 
+
+
+#COMPUTER CHOICE    
+
+#import random
+
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print("Computer Choise:", computer_choice)
